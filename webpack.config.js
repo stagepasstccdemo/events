36478859f3f7c78360f7813270675056d0d2ac92
fillipeags <fillipe484@gmail.com>
2023-03-26 19:59:56 -0300

    chore: setup service-worker plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
@@ -11,5 +12,13 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
+    // add plugin to allow service worker to be exported
+    plugins: [
+      new WorkboxPlugin.GenerateSW({
+        clientsClaim: true,
+        skipWaiting: true,
+        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 10MB
+      }),
+    ],
   });
 };
