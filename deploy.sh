775ad8e7e9fda9777bff5dbf309725a4183074e4
fillipeags <fillipe484@gmail.com>
2023-03-26 03:35:44 -0300

    chore: setup github action deploy hook to upload the app build

--- deploy.sh
+++ deploy.sh
@@ -7,14 +7,14 @@ chmod a+x ./jq
 # Get version from package.json
 VERSION=$(node --eval="process.stdout.write(require('./package.json').version)") 
 
-# # Get the current import-map.json
-# aws s3 cp s3://mfe-stage-pass/config/import-map.json ./import-map.json
+# Get the current import-map.json
+aws s3 cp s3://mfe-stage-pass/config/import-map.json ./import-map.json
 
-# # Update the import-map.json
-# NEW_URL=/config/mfe/app-events/$VERSION/stagepass-app-events.js
+# Update the import-map.json
+NEW_URL=/config/mfe/app-events/$VERSION/stagepass-app-events.js
 
-# # Update the import-map.json
-# cat ./import-map.json | ./jq --arg NEW_URL "$NEW_URL" '.imports["@stagepass/app-events"] = $NEW_URL' > new.importmap.json
+# Update the import-map.json
+cat ./import-map.json | ./jq --arg NEW_URL "$NEW_URL" '.imports["@stagepass/app-events"] = $NEW_URL' > new.importmap.json
 
 # Upload the new import-map.json
 aws s3 cp dist s3://mfe-stage-pass/config/mfe/app-events/$VERSION --recursive
