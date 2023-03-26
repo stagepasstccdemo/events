2e136d24290c876104fb0a82dcb088838edb5a82
fillipeags <fillipe484@gmail.com>
2023-03-26 03:55:10 -0300

    chore: update deploy sh info

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ aws s3 cp s3://mfe-stage-pass/config/import-map.json import-map.json
 NEW_URL=/config/mfe/app-events/$VERSION/stagepass-app-events.js
 
 # Update the import-map.json
-test -s ./import-map.json && cat ./import-map.json | ./jq --arg NEW_URL "$NEW_URL" '.imports["@stagepass/app-events"] = $NEW_URL' > new.importmap.json || echo '{}' > new.importmap.json
+test -s ./import-map.json && cat ./import-map.json | ./jq --arg NEW_URL "$NEW_URL" '.imports["@stagepass/app-events"] = $NEW_URL' > new.importmap.json || echo '{"imports": {"@stagepass/app-events": "'"$NEW_URL"'"}}' > new.importmap.json
 
 # Upload the new import-map.json
 aws s3 cp dist s3://mfe-stage-pass/config/mfe/app-events/$VERSION --recursive
