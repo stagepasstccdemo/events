316b5336ff32dc08f9161a7b2faac96c41899ed7
fillipeags <fillipe484@gmail.com>
2023-03-26 06:54:47 -0300

    chore: setup sentry for error monitoring

--- src/stagepass-app-events.tsx
+++ src/stagepass-app-events.tsx
@@ -1,14 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
+
+import * as Sentry from "@sentry/browser";
+import { Integrations } from "@sentry/tracing";
 import Root from "./root.component";
+
+// Change to env variable later on
+
+Sentry.init({
+  dsn: "https://af23a7def5a74172a8d87d2e0e78c053@o4504899977936896.ingest.sentry.io/4504904055324672",
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 const lifecycles = singleSpaReact({
   React,
   ReactDOM,
   rootComponent: Root,
   errorBoundary(err, info, props) {
-    // Customize the root error boundary for your microfrontend here.
+    Sentry.withScope((scope) => {
+      scope.setExtra("props", props);
+      scope.setExtra("info", info);
+      Sentry.captureException(err);
+    });
+
     return null;
   },
 });
