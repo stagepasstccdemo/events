643de2190a0e8f3b30a33a86af5755a3812a8fac
Fillipe Gomes <fillipe.gomes@kartado.com.br>
2023-04-07 19:56:13 -0300

    chore: add util ui and state management

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,23 @@
+// @ts-ignore
+import { Navigate } from "@stagepass/osiris-ui";
+
+// @ts-ignore
+import { useStore } from "@stagepass/util-state";
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  const store = useStore();
+
+  return (
+    <>
+      <section>{props.name} is mounted!</section>
+      <h3>COunter: {store.counter}</h3>
+      <button type="button" onClick={store.increment}>
+        Aumentar
+      </button>
+      <button type="button" onClick={store.decrement}>
+        Diminuir
+      </button>
+      <Navigate path="/teste">Teste</Navigate>
+    </>
+  );
 }
