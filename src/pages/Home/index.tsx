d6b53dfec0fd0eb8bb2dfcfc7a65431ff9ceeaf8
Fillipe Gomes <fillipe.gomes@kartado.com.br>
2023-04-25 12:30:37 -0300

    feat: creates card

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,6 +6,11 @@ import {
   SearchInput,
   Filter,
   FilteredCards,
+  Button,
+  Image,
+  Text,
+  Flex,
+  SimpleDivider,
 } from "@stagepass/osiris-ui";
 import { FiMenu, FiFilter, MdFilterList } from "@assets/icons";
 import LogoImg from "@assets/logo-inline.png";
@@ -33,8 +38,61 @@ export function Home() {
           renderBadgesList={filterBadgesList}
         />
 
-        <Box mt="32px">
+        <Box mt="2rem">
           <FilteredCards textLabel="Filtered Options" buttonLabel="show all" />
+          <Flex
+            mt="2rem"
+            width="250px"
+            maxHeight="360px"
+            rounded="15px"
+            bgColor="os-secondary.100"
+            flexDirection="column"
+            justifyContent="space-between"
+          >
+            <Image
+              src="https://musicmayhemmagazine.com/wp-content/uploads/2021/03/Screen-Shot-2021-03-04-at-10.11.52-PM.png"
+              roundedTop={15}
+            />
+            <Box p="4">
+              <Text
+                text="Justin Bieber | #PeachesTour"
+                color="os-primary.300"
+                fontWeight="bold"
+                fontSize="md"
+                noOfLines={1}
+              />
+              <Text
+                text="December 20 & 22 | 5PM - 7PM"
+                color="gray.100"
+                fontWeight="bold"
+                fontSize="xs"
+                noOfLines={1}
+              />
+              <Text
+                text="Allianz Parque Stadium, Sao Paulo, Brazil"
+                color="gray.300"
+                fontWeight="light"
+                fontSize="xs"
+                noOfLines={1}
+              />
+              <SimpleDivider numDots={10} padding="14px 0px" />
+              <Text
+                text="Starting at $200"
+                color="os-primary.300"
+                fontWeight="bold"
+              />
+            </Box>
+            <Button
+              bgColor="os-ternary.300"
+              color="gray.100"
+              height="45px"
+              roundedBottom="15px"
+              px="48px"
+              py="28px"
+            >
+              GET TICKET RIGHT NOW
+            </Button>
+          </Flex>
         </Box>
       </Box>
     </BaseContainer>
