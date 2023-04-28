cc1a1824d6793562bade97e538452cfa45c28571
Fillipe Gomes <fillipe.gomes@kartado.com.br>
2023-04-28 01:21:01 -0300

    feat: update mocks for card items

--- src/mocks/filteredCardListItems.ts
+++ src/mocks/filteredCardListItems.ts
@@ -10,4 +10,15 @@ export const filteredCardListItems = [
     textButtonLabel: "GET TICKETS RIGHT NOW",
     parentalRating: "18+",
   },
+  {
+    key: 2,
+    sourceBannerImg:
+      "https://musicmayhemmagazine.com/wp-content/uploads/2021/03/Screen-Shot-2021-03-04-at-10.11.52-PM.png",
+    titleCard: "Justin Bieber | #PeachesTour",
+    legendCard: "December 20 & 22 | 5PM - 7PM",
+    detailsDescriptionCard: "Allianz Parque Stadium, Sao Paulo, Brazil",
+    priceTag: "Starting at $200",
+    textButtonLabel: "GET TICKETS RIGHT NOW",
+    parentalRating: "18+",
+  },
 ];
